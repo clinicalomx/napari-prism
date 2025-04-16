4337c17a8722db316fcec103591027b26b25a86a
rtubelleza <rafaelrtubelleza@gmail.com>
2025-04-16 17:52:50 +1000

    Add .gr for spatial functions, update full docs

--- src/napari_prism/__init__.py
+++ src/napari_prism/__init__.py
@@ -3,6 +3,6 @@ from importlib.metadata import version
 
 __version__ = version("napari-prism")
 
-from . import datasets, im, io, pl, pp, tl
+from . import datasets, gr, im, io, pl, pp, tl
 
-__all__ = ["im", "pp", "tl", "im", "pl", "io", "datasets"]
+__all__ = ["im", "pp", "tl", "im", "pl", "io", "datasets", "gr"]
