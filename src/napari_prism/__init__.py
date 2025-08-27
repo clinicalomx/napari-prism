f3ccd4d1d9fc1e62886d045d034dbbf2fcb0a875
rtubelleza <rafaelrtubelleza@gmail.com>
2025-08-27 14:12:27 +1000

    Fix package metadata

--- src/napari_prism/__init__.py
+++ src/napari_prism/__init__.py
@@ -1,8 +1,8 @@
-# __version__ = "0.1"
-from importlib.metadata import version
-
-__version__ = version("napari-prism")
+try:
+    from ._version import __version__
+except ImportError:
+    __version__ = "unknown"
 
 from . import datasets, gr, im, io, pl, pp, tl
 
-__all__ = ["im", "pp", "tl", "im", "pl", "io", "datasets", "gr"]
+__all__ = ["im", "pp", "tl", "pl", "io", "datasets", "gr"]
